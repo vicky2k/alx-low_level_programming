2f06e3a51bf8b750ff496cef0e824aba46b242f1
vicky2k <vicspidin@gmail.com>
2022-06-30 10:17:23 -0700

    use function puts to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /**
- *
  *  * main - main block to execute
- *
  *   * Return: 0 for int
- *
- *    */
-
-int main(void)
-
+ */
+int main(void)	
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

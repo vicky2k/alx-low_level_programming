c4cd73912624631b9d5c614045592a9433d3d058
vicky2k <vicspidin@gmail.com>
2022-06-30 10:00:24 -0700

    c is fun and puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - main block to execute
+ *
+ *   * Return: 0 for int
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}

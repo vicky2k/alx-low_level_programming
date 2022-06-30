ac53d6ab034ca574bac1987c4f498a14b958b1a5
vicky2k <vicspidin@gmail.com>
2022-06-30 10:25:48 -0700

    function printf followed by new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block to be executed
+ * Return: 0 for int
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

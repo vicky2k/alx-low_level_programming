e65de51e81366c6d88c750178ed10bc3fa5662a7
vicky2k <vicspidin@gmail.com>
2022-07-07 06:49:51 -0700

    betty compliant task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -7,9 +8,8 @@
 int main(void)
 {
 	int i;
-	printf("Infinite loop incoming :(\n");
-	
-	i = 0;
+printf("Infinite loop incoming :(\n");
+i = 0;
 
 /*
  *while (i < 10)

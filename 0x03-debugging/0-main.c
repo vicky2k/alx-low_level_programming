f6497d6bdd79e906b978a658d34b23888539dcfa
vicky2k <vicspidin@gmail.com>
2022-07-07 03:49:59 -0700

    still on task 0 debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,3 +1,33 @@
+#include <stdlib.h>                                                                                                                   
+#include <time.h>                                                                                                                       
+#include <stdio.h>                                                                                                                      
+/**                                                                                                                                     
+ * main - lock to be executes                                                                                                           
+ * Return: 0-success                                                                                                                    
+ */                                                                                                                                     
+void positive_or_negative(int i)
+{
+	int n;                                                                                                                                  
+	srand(time(0));                                                                                                                         
+	n = i;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{		
+		printf("%d is zero\n", n);
+	}		
+	else if (n < 0)
+	{	
+		printf("%d is negative\n", n);
+	}
+	return (0);
+} 
+
+
+
 #include "main.h"
 
 /**
@@ -12,5 +42,4 @@ int i;
 i = 0;
 positive or negative(i);
 
-return (0);
 }

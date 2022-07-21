ae79a4a9d13b467cf38492ff488e68bc22dc5044
vicky2k <vicspidin@gmail.com>
2022-07-20 19:17:54 -0700

    return value x to power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - search a string for any ofa set of bytes
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in s that matches one of the bytes in accept
+ * or NULL if no byte is found
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
